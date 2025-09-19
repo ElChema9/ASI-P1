1eed97d891bb6d2e95c520ddc1351d8bfbea9d44
Álex Cortiñas <mail@alexcortinas.es>
2025-09-19 10:06:03 +0200

    Eliminando código que sobra en la versión actual
    (se incluirá de nuevo al implementar imágenes)

--- server/src/main/java/es/udc/asi/notebook_rest/config/Properties.java
+++ server/src/main/java/es/udc/asi/notebook_rest/config/Properties.java
@@ -12,7 +12,6 @@ public class Properties {
   private String clientHost;
   private String jwtSecretKey;
   private Long jwtValidity;
-  private String rutaImagenes;
 
   public String getClientHost() {
     return clientHost;
@@ -37,14 +36,6 @@ public class Properties {
 
   public void setJwtValidity(Long jwtValidity) {
     this.jwtValidity = jwtValidity;
-  }
-
-  public String getRutaImagenes() {
-    return rutaImagenes;
-  }
-
-  public void setRutaImagenes(String rutaImagenes) {
-    this.rutaImagenes = rutaImagenes;
   }
 
 }
